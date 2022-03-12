f1bf01da897b29bce68d9cc6a0c946d6c7db95a0
sgjzfzzf <sgjzfzzf@sjtu.edu.cn>
2022-03-12 03:23:41 -0800

    finish lab 1

--- project1/calc.c
+++ project1/calc.c
@@ -4,7 +4,7 @@
 #include <linux/uaccess.h>
 
 #define MAX_SIZE 128
-#define ID "<Your Student ID>"
+#define ID "519030910182"
 
 static int operand1;
 module_param(operand1, int, 0);
@@ -21,12 +21,73 @@ int out_len;
 
 static ssize_t proc_read(struct file *fp, char __user *ubuf, size_t len, loff_t *pos)
 {
-    /* TODO */
+    int tmp[MAX_SIZE], p = 0, i, j, num;
+    char buf[MAX_SIZE];
+    memset((void *)output, 0, sizeof(output));
+    if (*pos > 0)
+    {
+        return 0;
+    }
+    if (len < MAX_SIZE)
+    {
+        return -EFAULT;
+    }
+    if (strcmp(operator, "add") == 0)
+    {
+        for (i = 0; i < ninp; ++i)
+        {
+            tmp[i] = operand1 + operand2[i];
+        }
+    }
+    else if (strcmp(operator, "mul") == 0)
+    {
+        for (i = 0; i < ninp; ++i)
+        {
+            tmp[i] = operand1 * operand2[i];
+        }
+    }
+    for (i = 0; i < ninp; ++i)
+    {
+        for (num = tmp[i]; num > 0; num /= 10, ++p)
+        {
+            memset((void *)buf, 0, sizeof(buf));
+            for (j = 0; num > 0; num /= 10, ++j)
+            {
+                buf[j] = num % 10 + '0';
+            }
+            for (--j; j >= 0; --j)
+            {
+                output[p++] = buf[j];
+            }
+        }
+        output[p++] = ',';
+    }
+    output[--p] = '\n';
+    ++p;
+    output[p++] = '\0';
+    if (copy_to_user((void *)ubuf, (void *)output, p))
+    {
+        return -EFAULT;
+    }
+    *pos = p;
+    return p;
 }
 
 static ssize_t proc_write(struct file *fp, const char __user *ubuf, size_t len, loff_t *pos)
 {
-    /* TODO */
+    int num = 0, i;
+    memset((void *)output, 0, sizeof(output));
+    if (*pos > 0 || len > MAX_SIZE || copy_from_user(output, ubuf, len))
+    {
+        return -EFAULT;
+    }
+    for (i = 0; i < len - 1; ++i)
+    {
+        num *= 10;
+        num += (output[i] - '0');
+    }
+    operand1 = num;
+    return len;
 }
 
 static const struct proc_ops proc_ops = {
@@ -36,12 +97,23 @@ static const struct proc_ops proc_ops = {
 
 static int __init proc_init(void)
 {
-    /* TODO */
+    proc_dir = proc_mkdir(ID, NULL);
+    proc_ent = proc_create("calc", 0666, proc_dir, &proc_ops);
+    if (proc_dir && proc_ent)
+    {
+        return 0;
+    }
+    else
+    {
+        proc_remove(proc_ent);
+        return -ENOMEM;
+    }
 }
 
 static void __exit proc_exit(void)
 {
-    /* TODO */
+    proc_remove(proc_dir);
+    proc_remove(proc_ent);
 }
 
 module_init(proc_init);
--- project1/ps.c
+++ project1/ps.c
@@ -1,9 +1,106 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(void)
+#define PATHBUFFERSIZE 0x100
+#define STRBUFFERSIZE 0x10000
+
+int isValidPid(char *pid)
 {
-    /* TODO */
+    for (int i = 0; pid[i] != '\0'; ++i)
+    {
+        if (pid[i] < '0' || pid[i] > '9')
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+char getS(char *stat)
+{
+    int spaceCount = 0, i = 0;
+    for (; spaceCount < 2; ++i)
+    {
+        if (stat[i] == ' ')
+        {
+            ++spaceCount;
+        }
+    }
+    return stat[i];
+}
+
+int main(int argc, char *argv[])
+{
+    char pathbuf[PATHBUFFERSIZE] = "/proc", strbuf[STRBUFFERSIZE];
+    int index1 = strlen(pathbuf);
+    DIR *dir = opendir(pathbuf);
+    struct dirent *dirent;
+    if (dir == NULL)
+    {
+        fprintf(stderr, "cannot open proc\n");
+        return -1;
+    }
+    printf("  PID S CMD\n");
+    while ((dirent = readdir(dir)))
+    {
+        if (dirent->d_type == 4 && isValidPid(dirent->d_name))
+        {
+            pathbuf[index1] = '/';
+            strcpy(pathbuf + index1 + 1, dirent->d_name);
+            int index2 = strlen(pathbuf), pidlen = strlen(dirent->d_name);
+            DIR *subdir = opendir(pathbuf);
+            if (pidlen < 5 && subdir != NULL)
+            {
+                pathbuf[index2] = '/';
+                for (int i = 0; i < 5 - pidlen; ++i)
+                {
+                    printf(" ");
+                }
+                printf("%s", dirent->d_name);
+                strcpy(pathbuf + index2 + 1, "stat");
+                memset((void *)strbuf, 0, sizeof(strbuf));
+                int fd = open(pathbuf, O_RDONLY);
+                if (fd > 0)
+                {
+                    if (read(fd, strbuf, STRBUFFERSIZE) != STRBUFFERSIZE)
+                    {
+                        printf(" %c ", getS(strbuf));
+                    }
+                }
+                close(fd);
+                memset((void *)strbuf, 0, sizeof(strbuf));
+                int flag = 0;
+                strcpy(pathbuf + index2 + 1, "cmdline");
+                if ((fd = open(pathbuf, O_RDONLY)) >= 0)
+                {
+                    int len = read(fd, strbuf, STRBUFFERSIZE);
+                    if (len != 0 && len != STRBUFFERSIZE)
+                    {
+                        printf("%s", strbuf);
+                        flag = 1;
+                    }
+                    close(fd);
+                }
+                strcpy(pathbuf + index2 + 1, "comm");
+                memset((void *)strbuf, 0, sizeof(strbuf));
+                if (!flag && (fd = open(pathbuf, O_RDONLY)) >= 0)
+                {
+                    if (read(fd, strbuf, STRBUFFERSIZE) != STRBUFFERSIZE)
+                    {
+                        strbuf[strlen(strbuf) - 1] = '\0';
+                        printf("[%s]", strbuf);
+                    }
+                    close(fd);
+                }
+                printf("\n");
+            }
+            closedir(subdir);
+        }
+    }
+    closedir(dir);
     return 0;
 }
